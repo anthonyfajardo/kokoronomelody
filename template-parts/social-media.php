e67d0dfee21030f16f3a9fa5212a68553a0fc3fb
Anthony Fajardo <anthonyfajardo@gmail.com>
2019-11-07 01:03:02 -0500

    fixed linear gradient transition on instagram button from hover to opacity changing since linear-gradients apparently can't use transitions

--- template-parts/social-media.php
+++ template-parts/social-media.php
@@ -12,7 +12,7 @@
 			 
 			 <?php if(have_rows('social_media_list')): ?>
 			 	<?php while(have_rows('social_media_list')): the_row(); ?>
-					<a href="<?php the_sub_field('link'); ?>" class="<?php echo strtolower(get_sub_field('name')); ?>">
+					<a href="<?php the_sub_field('link'); ?>" class="<?php echo strtolower(get_sub_field('name')).'-button'; ?>">
 						<?php the_sub_field('icon'); ?>
 					</a>
 					
